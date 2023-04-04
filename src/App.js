efeae152367c2f0c70f33bea5a01dcd1dbade2e6
Jokemon-coder <Jokemon-coder@users.noreply.github.com>
2023-04-04 12:00:46 +0300

    Navbar component and some designing and creating on the 'home page'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import LoginRegister from './pages/login/login-register';
 import Home from './pages/home/home';
+import Navbar from './components/Navbar/Navbar';
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -60,6 +61,7 @@ function App() {
 
   return (
     <div className="App" onLoad={check()}>
+      <Navbar/>
       <Routes>
       <Route exact path="/" element={<Home logged={checkLogged} setChanged={setLoggedState} click={LogInOut}/>}/>
       <Route exact path="/login" element={<LoginRegister logged={checkLogged} setChanged={setLoggedState} click={LogInOut}/>}/>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+
+
+
+
+
+    return(
+        <React.Fragment>
+            <ul id="Navbar">
+                <li className="NavItem">Example</li>
+                <li className="NavItem">Example</li>
+                <li className="NavItem">Example</li>
+                <li className="NavItem">Example</li>
+            </ul>
+        </React.Fragment>
+    );
+}
+
+export default Navbar;
